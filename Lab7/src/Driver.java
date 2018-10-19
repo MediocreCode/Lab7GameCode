7c231cacd20f3d457581ab9088d34193a3c3f60a
Raleigh Dial <raleigh.dial@gmail.com>
2018-10-19 16:09:14 -0600

    Added Driver(actually) and same Bug

---
+++ Lab7/src/Driver.java
@@ -0,0 +1,17 @@
+
+public class Driver {
+/*
+ * Ya boi R-Dog
+ * 10/19
+ */
+	public static void main(String[] args) {
+		CoinItem c1 = new CoinItem();
+		System.out.println(c1.genCoinX()+" "+c1.genCoinY());
+		HomeScreenGraphic hsg = new HomeScreenGraphic();
+		//String str = new String(hsg.getPlayerName()); WE GOT A BUG WOOOOOOO!
+		//System.out.println(str);
+		
+
+	}
+
+}
